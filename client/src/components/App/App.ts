be3b0aad777a1096e50dee86f45550f066627154
lgklsv <kolosov20011@gmail.com>
2023-01-27 12:28:24 +0300

    feat: add sticky footer component and make credentials render function

--- client/src/components/App/App.ts
+++ client/src/components/App/App.ts
@@ -1,4 +1,4 @@
-// import { renderFooter } from '../Footer/Footer';
+import { renderFooter } from '../Footer/Footer';
 import { renderHeader } from '../Header/Header';
 import styles from './App.module.scss';
 
@@ -10,11 +10,10 @@ export const renderApp = (func: () => HTMLElement): Element => {
   appContiner.innerHTML = '';
 
   const header: HTMLElement = renderHeader();
-  // const footer: HTMLElement = renderFooter();
+  const footer: HTMLElement = renderFooter();
   const main: HTMLElement = func();
 
-  // appContiner.append(header, main, footer);
-  appContiner.append(header, main);
+  appContiner.append(header, main, footer);
 
   return appContiner;
 };
---
+++ client/src/components/Footer/components/Credentials/Credentials.ts
@@ -0,0 +1,17 @@
+import { createElem } from '../../../../utils/create-element';
+import { createLink } from '../../../../utils/create-link-element';
+
+export const rederCredentials = (nicknames: string[]): HTMLElement => {
+  const footerCredentials: HTMLElement = createElem('div', 'footer__credentials');
+  footerCredentials.innerHTML = 'rs clone engineered by ';
+
+  nicknames.forEach((name, idx) => {
+    const footerGitLink: HTMLElement = createLink(`https://github.com/${name}`, 'footer__link', true, name);
+    footerCredentials.append(footerGitLink);
+    if (idx !== nicknames.length - 1) {
+      footerCredentials.innerHTML = `${footerCredentials.innerHTML} & `;
+    }
+  });
+
+  return footerCredentials;
+};
