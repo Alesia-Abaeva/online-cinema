48bfdcc05ad78fa7d76298c06d0f08d5efa34653
lgklsv <kolosov20011@gmail.com>
2023-01-27 11:45:42 +0300

    feat: implement main page container

--- client/src/components/MainPage/MainPage.ts
+++ client/src/components/MainPage/MainPage.ts
@@ -3,8 +3,12 @@ import styles from './MainPage.module.scss';
 
 export const renderMainPage = (): HTMLElement => {
   const main: HTMLElement = createElem('main', styles['main']);
+
+  const mainContainer: HTMLElement = createElem('div', 'main__container');
   const header: HTMLElement = document.createElement('h1');
   header.innerHTML = 'Do u like the font ?';
-  main.append(header);
+  mainContainer.append(header);
+
+  main.append(mainContainer);
   return main;
 };
