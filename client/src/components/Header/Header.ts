e2dcb8a218640cde2c77bd039dbce400280185a8
lgklsv <kolosov20011@gmail.com>
2023-01-28 12:58:48 +0300

    feat: add header navbar search and login btns with corresponding links

--- client/src/components/Header/Header.ts
+++ client/src/components/Header/Header.ts
@@ -1,5 +1,8 @@
 import { createElem } from '../../utils/create-element';
+import { createLink } from '../../utils/create-link-element';
 import styles from './Header.module.scss';
+import { linkHandler } from '../../utils/link-handler';
+import { rednerNavbar } from './components/Navbar/Navbar';
 
 export const renderHeader = (): HTMLElement => {
   const header: HTMLElement = createElem('header', 'header');
@@ -7,12 +10,24 @@ export const renderHeader = (): HTMLElement => {
   const headerContainer: HTMLElement = createElem('div', 'header__container');
 
   const logo: HTMLElement = createElem('div', 'header__logo');
-  const logoLink: HTMLElement = createElem('a', styles['header-logo__link']);
+  const logoLink: HTMLElement = createElem('a', styles['header__logo-link']);
   logoLink.setAttribute('href', '/');
-
   logo.append(logoLink);
 
-  headerContainer.append(logo);
+  logoLink.onclick = linkHandler;
+
+  const navBar: HTMLElement = rednerNavbar();
+
+  const accoutSection: HTMLElement = createElem('div', 'header__account');
+  const loginBtn: HTMLElement = createElem('div', 'header__login');
+  const loginLink: HTMLElement = createLink('/login', 'header__login-link', false, 'Войти');
+  loginBtn.append(loginLink);
+
+  loginLink.onclick = linkHandler;
+
+  accoutSection.append(loginBtn);
+
+  headerContainer.append(logo, navBar, accoutSection);
   header.append(headerContainer);
 
   return header;
