5ed895e10528aaf670baa49b109e71ded83473a0
Alesia <alesia.litvinenko@gmail.com>
2023-01-28 20:57:33 +0300

    feat: add basic settings & test structure redux

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,2 +1,10 @@
 import './style.scss';
 import './router/router';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { rootReducer } from './logic/redux/root-reduces';
+import logger from 'redux-logger';
+
+// TODO - добавить стор в функцию Init
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+console.log(store);
---
+++ client/src/logic/redux/root-reduces.ts
@@ -0,0 +1,26 @@
+import { combineReducers } from 'redux';
+import { DISABLE, ENABLE } from './types-redux';
+
+const initialTheme = {
+  value: 'ligth',
+  disabled: false,
+}; // тестовое начальное значение - TODO - изменить и перенести
+
+function themeReducer(state = initialTheme, action: TypesRedux) {
+  switch (action.type) {
+    case ENABLE:
+      return { ...state, disabled: false };
+    case DISABLE:
+      return { ...state, disabled: true };
+    default:
+      return state;
+  }
+  return state;
+} // TODO - поменять под наш функционал ТЕСТ
+
+// Должны проверять какой action нам приходит и менять состояние
+
+// rootReducer - изменяет все наши состояния
+export const rootReducer = combineReducers({
+  theme: themeReducer,
+});
