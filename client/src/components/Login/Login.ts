0adfb006cc22b4f4731b57e8602d50397923df9b
Alesia <alesia.litvinenko@gmail.com>
2023-01-31 00:40:18 +0300

    feat: change login page & add new style

--- client/src/components/Login/Login.ts
+++ client/src/components/Login/Login.ts
@@ -1,7 +1,9 @@
 import { registerHandler } from 'src/api/back/auth';
 import { createElem } from 'src/utils/create-element';
+import { createLink } from 'src/utils/create-link-element';
 import { createButton } from '../ui/Button/Button';
 import { createInputElement } from '../ui/Input/Input';
+import { mailIcon, passwordIcon } from './icons';
 import styles from './Login.module.scss';
 
 export const renderLoginPage = (): HTMLElement => {
@@ -15,42 +17,54 @@ export const renderLoginPage = (): HTMLElement => {
   const mainContainer: HTMLElement = createElem('div', 'main__container');
   const mainContent: HTMLElement = createElem('div', styles['login-page']);
 
-  const header: HTMLElement = document.createElement('h1');
-  header.innerHTML = 'Войдите или зарегистрируйтесь';
-
   const formContainer = createElem('div', 'form_container');
 
-  const wrapperEmail = createElem('div', 'form__wrapp');
+  const logo = createElem('div', 'form_logo');
+
+  const wrapperEmail = createElem('form', 'form__wrapp');
   const labelEmail = createElem('label', 'form_label');
-  const inputEmail = createInputElement({ type: 'text', placeholder: 'Введите email', id: 'email', name: 'email' });
+  const iconEmail = createElem('div', 'icon__container');
+  iconEmail.innerHTML = mailIcon;
+  const inputEmail = createInputElement({ type: 'email', placeholder: 'Введите email', id: 'email', name: 'email' });
   inputEmail.oninput = () => {
     stateInput.email = inputEmail.value;
   };
 
-  wrapperEmail.append(labelEmail, inputEmail);
+  wrapperEmail.append(labelEmail, iconEmail, inputEmail);
 
-  const wrapperPas = createElem('div', 'form__wrapp');
+  const wrapperPas = createElem('form', 'form__wrapp');
   const labelPas = createElem('label', 'form_label');
+  const iconPass = createElem('div', 'icon__container');
+  iconPass.innerHTML = passwordIcon;
   const inputPas = createInputElement({
     type: 'password',
     placeholder: 'Введите пароль',
     id: 'password',
     name: 'password',
   });
+  // inputPas.setAttribute('minLength', '6'); Добавить эту проверку при регистрации
   inputPas.oninput = () => {
     stateInput.password = inputPas.value;
   };
 
-  wrapperPas.append(labelPas, inputPas);
+  wrapperPas.append(iconPass, labelPas, inputPas);
 
-  const button = createButton('Войти', () => {
-    registerHandler({ email: stateInput.email, password: stateInput.password });
-  });
+  const button = createButton(
+    'Войти',
+    () => {
+      registerHandler({ email: stateInput.email, password: stateInput.password });
+    },
+    'form_button'
+  );
 
-  // const buttonReg = createButton('Регистрация');
+  const registrationContainer = createElem('div', 'reg__container');
+  const registrationLink = createLink('/register', 'reg__link', true, 'Зарегистрироваться');
+  const registrationText = createElem('div', 'reg__text');
+  registrationText.innerHTML = `, если у вас не аккаунта.`;
+  registrationContainer.append(registrationLink, registrationText);
 
-  formContainer.append(wrapperEmail, wrapperPas, button);
-  mainContent.append(header, formContainer);
+  formContainer.append(logo, wrapperEmail, wrapperPas, button, registrationContainer);
+  mainContent.append(formContainer);
   mainContainer.append(mainContent);
   main.append(mainContainer);
 
