1185b47c47fcb7097282181ea63d7127bb2c2172
lgklsv <kolosov20011@gmail.com>
2023-02-03 17:50:03 +0300

    feat: add light theme config

--- client/src/components/Footer/components/ThemeSwitcher/ThemeSwitcher.ts
+++ client/src/components/Footer/components/ThemeSwitcher/ThemeSwitcher.ts
@@ -5,7 +5,7 @@ export const renderThemeSwitcher = (): HTMLElement => {
   const themeSwitcher: HTMLElement = createElem('div', styles['theme-switcher']);
 
   const lightBtn: HTMLElement = createElem('button', 'theme-switcher__btn');
-  lightBtn.dataset.theme = 'light';
+  // lightBtn.dataset.theme = 'light';
   const sunIcon: HTMLElement = createElem('div', 'theme-switcher__icon');
   sunIcon.classList.add('sun-icon');
   const lightBtnText: HTMLElement = createElem('span', 'theme-switcher__text');
@@ -14,7 +14,7 @@ export const renderThemeSwitcher = (): HTMLElement => {
   lightBtn.append(sunIcon, lightBtnText);
 
   const darkBtn: HTMLElement = createElem('button', 'theme-switcher__btn');
-  darkBtn.dataset.theme = 'dark';
+  // darkBtn.dataset.theme = 'dark';
   darkBtn.classList.add('theme-switcher__btn_active');
   const moonIcon: HTMLElement = createElem('div', 'theme-switcher__icon');
   moonIcon.classList.add('moon-icon');
