ec7009210b347aa58f5776a499347a32f5102d76
lgklsv <kolosov20011@gmail.com>
2023-02-04 16:49:26 +0300

    feat: add subscription plans card component and style it

--- client/src/components/SubscriptionsPage/SubscriptionsPage.ts
+++ client/src/components/SubscriptionsPage/SubscriptionsPage.ts
@@ -1,14 +1,23 @@
+import { SUBSCRIPTION_PLANS } from 'src/const/subscriptions-data';
 import { createElem } from 'src/utils/create-element';
+import { renderTariffCard } from './components/TariffCard/TariffCard';
 import styles from './SubscriptionsPage.module.scss';
 
 export const renderSubscriptions = (): HTMLElement => {
   const main: HTMLElement = createElem('main', 'main');
+  main.classList.add('main_backdrop');
   const mainContainer: HTMLElement = createElem('div', 'main__container');
   const mainContent: HTMLElement = createElem('div', styles['subscriptions']);
 
   const subsTitle: HTMLElement = createElem('h1', 'subscriptions__title');
-  subsTitle.innerHTML = 'Выберите тариф';
+  subsTitle.innerHTML = 'Выберите подписку';
   const subsTable: HTMLElement = createElem('div', 'subscriptions__body');
+
+  SUBSCRIPTION_PLANS.forEach((el, id) => {
+    const subsCard: HTMLElement = renderTariffCard(el, id);
+    if (id === 1) subsCard.classList.add('tariff-card_active');
+    subsTable.append(subsCard);
+  });
 
   mainContent.append(subsTitle, subsTable);
 
---
+++ client/src/components/SubscriptionsPage/components/TariffCard/TariffCard.ts
@@ -0,0 +1,51 @@
+import { createButton } from 'src/components/ui/Button/Button';
+import { createElem } from 'src/utils/create-element';
+import styles from './TariffCard.module.scss';
+
+export const renderTariffCard = (data: SubsctiptionsPlan, id: number): HTMLElement => {
+  const tariffCard: HTMLElement = createElem('div', styles['tariff-card']);
+  const cardHeader: HTMLElement = createElem('div', 'tariff-card__header');
+
+  const cardTitle: HTMLElement = createElem('h2', 'tariff-card__title');
+  cardTitle.innerHTML = data.title;
+
+  const cardLine: HTMLElement = createElem('div', 'tariff-card__line');
+
+  const cardCost: HTMLElement = createElem('div', 'tariff-card__cost');
+
+  const cardCostAmount: HTMLElement = createElem('div', 'tariff-card__cost-amount');
+  cardCostAmount.innerHTML = `${data.cost}`;
+
+  const cardCostAmountDet: HTMLElement = createElem('div', 'tariff-card__cost-det');
+
+  const cardCostCurrency: HTMLElement = createElem('span', 'tariff-card__cost-currency');
+  cardCostCurrency.innerHTML = ' руб';
+  const cardCostPeriod: HTMLElement = createElem('span', 'tariff-card__cost-period');
+  cardCostPeriod.innerHTML = '/мес';
+
+  cardCostAmountDet.append(cardCostCurrency, cardCostPeriod);
+
+  cardCost.append(cardCostAmount, cardCostAmountDet);
+
+  cardHeader.append(cardTitle, cardLine, cardCost);
+
+  const cardBody: HTMLElement = createElem('div', 'tariff-card__body');
+  const cardList: HTMLElement = createElem('ul', 'tariff-card__list');
+  cardBody.append(cardList);
+
+  data.benefits.forEach((el) => {
+    const cardBenefit: HTMLElement = createElem('li', 'tariff-card__list-item');
+    if (el.included) cardBenefit.classList.add('tariff-card__list-item_included');
+    else cardBenefit.classList.add('tariff-card__list-item_notincluded');
+    cardBenefit.innerHTML = el.title;
+    cardList.append(cardBenefit);
+  });
+  tariffCard.append(cardHeader, cardBody);
+
+  if (id === 1) {
+    const cardBtn: HTMLElement = createButton('Оформить подиску', undefined, 'tariff-card__btn');
+    tariffCard.append(cardBtn);
+  }
+
+  return tariffCard;
+};
