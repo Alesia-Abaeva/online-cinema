4b44f64eefd7f97604a88a8ebd4223c411f3fa0c
Alesia <alesia.litvinenko@gmail.com>
2023-02-09 23:57:50 +0300

    feat: add state on main page

---
+++ client/src/components/MainPage/components/HeroSection/ForNewUsers/HeroSecNewUsers.ts
@@ -0,0 +1,27 @@
+import { createButton } from 'src/components/ui/Button/Button';
+import { PATH_NAMES } from 'src/const/path-names';
+import { route } from 'src/router/route';
+import { createElem } from 'src/utils/create-element';
+
+export const renderHeroSectionNewUser = (): HTMLElement => {
+  const container = createElem('div', 'subs-hero__contents-container');
+  // TODO: Поменять текст на наш
+  const h1: HTMLElement = createElem('h1', 'subs-hero__title');
+  h1.innerHTML = 'Фильмы и сериалы по подписке';
+  const subTitle: HTMLElement = createElem('h2', 'subs-hero__sub-title');
+  subTitle.innerHTML =
+    'Тысячи фильмов и сериалов по цене одного на целый месяц: мировые блокбастеры, культовые сериалы, анимация от Disney';
+
+  const subsBtn: HTMLElement = createButton(
+    'Оформить подписку',
+    () => {
+      route(PATH_NAMES.register);
+    },
+    'subs-hero__btn'
+  );
+  subsBtn.textContent = 'Оформить подписку';
+
+  container.append(h1, subTitle, subsBtn);
+
+  return container;
+};
--- client/src/components/MainPage/components/HeroSection/HeroSection.ts
+++ client/src/components/MainPage/components/HeroSection/HeroSection.ts
@@ -1,29 +1,27 @@
-import { createButton } from 'src/components/ui/Button/Button';
-import { route } from 'src/router/route';
+import { store } from 'src/logic/redux';
 import { createElem } from '../../../../utils/create-element';
+import { renderHeroSectionAuthUser } from './AuthUser/HeroSecAuthUser';
+import { renderHeroSectionNewUser } from './ForNewUsers/HeroSecNewUsers';
 import styles from './HeroSection.module.scss';
 
 export const renderHeroSection = (): HTMLElement => {
   const heroSection: HTMLElement = createElem('section', styles['subs-hero']);
 
-  // TODO: Поменять текст на наш
-  const h1: HTMLElement = createElem('h1', 'subs-hero__title');
-  h1.innerHTML = 'Фильмы и сериалы по подписке';
-  const subTitle: HTMLElement = createElem('h2', 'subs-hero__sub-title');
-  subTitle.innerHTML =
-    'Тысячи фильмов и сериалов по цене одного на целый месяц: мировые блокбастеры, культовые сериалы, анимация от Disney';
+  const forNewUsers: HTMLElement = renderHeroSectionNewUser();
 
-  // TODO: Если авторизован, ссылка на страницу выбора подписки, если нет, ссылка на логин
-  const subsBtn: HTMLElement = createButton(
-    'Оформить подписку',
-    () => {
-      route('/subscriptions');
-    },
-    'subs-hero__btn'
-  );
-  subsBtn.textContent = 'Оформить подписку';
+  // TODO: изменить эту функцию, чтобы рендерлся блок с видео
+  const authUser: HTMLElement = renderHeroSectionAuthUser();
 
-  heroSection.append(h1, subTitle, subsBtn);
+  store.subscribe(() => {
+    const userState = store.getState().auth.user;
+
+    if (userState.data === null) {
+      // если не авторизован
+      heroSection.append(forNewUsers);
+    } else {
+      heroSection.append(authUser);
+    }
+  });
 
   return heroSection;
 };
