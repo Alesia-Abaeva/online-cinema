db1f00f04aaeb9579a54be88bd4e07d87bd19127
Alesia <alesia.litvinenko@gmail.com>
2023-02-09 19:16:00 +0300

    feat: add accordion on main page

--- client/src/components/MainPage/MainPage.ts
+++ client/src/components/MainPage/MainPage.ts
@@ -1,9 +1,11 @@
 import { createElem } from 'src/utils/create-element';
+import { REFERENC_DESCRIP, REFERENC_TITLE } from 'src/const/referens';
 import { renderSlider } from './components/Slider/Slider';
 import { renderHeroSection } from './components/HeroSection/HeroSection';
 import styles from './MainPage.module.scss';
 import { genresData, mockData } from './mockData';
 import { addListenerCollection, addListenerSlideDown } from './sliderActions';
+import { Accordion } from '../ui/Accordion/Accordion';
 
 export const renderMainPage = (): HTMLElement => {
   const main: HTMLElement = createElem('main', styles['main']);
@@ -17,8 +19,10 @@ export const renderMainPage = (): HTMLElement => {
   const section1: HTMLElement = addListenerSlideDown(renderSlider(mockData, 'Слайдер 1'));
   const section2: HTMLElement = addListenerCollection(renderSlider(genresData, 'Слайдер 2'));
   const section3: HTMLElement = addListenerSlideDown(renderSlider(mockData, 'Слайдер 3'));
+  const accordion: HTMLElement = createElem('div', 'accordion-section');
+  Accordion(accordion, REFERENC_TITLE, REFERENC_DESCRIP);
 
-  main.append(mainContainer, section1, section2, section3);
+  main.append(mainContainer, section1, section2, section3, accordion);
 
   return main;
 };
