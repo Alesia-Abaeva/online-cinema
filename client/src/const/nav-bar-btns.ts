43272d342e9259be60ba044beff71df010d6a809
Alesia <alesia.litvinenko@gmail.com>
2023-02-09 22:28:31 +0300

    feat: add change header for auth|not auth users

--- client/src/const/nav-bar-btns.ts
+++ client/src/const/nav-bar-btns.ts
@@ -1,6 +1,12 @@
-export const NAVBAR_BTNS: NavbarBtns[] = [
+export const NAVBAR_BTNS_AUTH: NavbarBtns[] = [
   { link: '/', text: 'Главная' },
   { link: '/personal', text: 'Мое' },
+  { link: '/all-lists', text: 'Списки' },
+];
+
+export const NAVBAR_BTNS: NavbarBtns[] = [
+  { link: '/', text: 'Главная' },
+  // { link: '/personal', text: 'Мое' },
   { link: '/all-lists', text: 'Списки' },
 ];
 
