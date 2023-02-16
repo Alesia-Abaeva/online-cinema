2fadc92873a2a5c2112a26876e7cc8d1cfb964fd
Alesia <alesia.litvinenko@gmail.com>
2023-02-16 13:48:42 +0300

    refactor: child header update

--- client/src/components/Header/components/Account/components/Avatar/Avatar.ts
+++ client/src/components/Header/components/Account/components/Avatar/Avatar.ts
@@ -10,12 +10,16 @@ import user from 'assets/img/user.svg';
 import styles from './Avatar.module.scss';
 
 export const renderAvatar = (): HTMLElement => {
-  const userStateAvatar = store.getState().user.personal.data?.avatarUrl;
+  const userStateOnLoad = store.getState().user.personal.data;
   const avatarWrap: HTMLElement = createElem('div', 'avatar__wrapper');
   const avatarCircle: HTMLElement = createElem('div', 'avatar__circle');
   const avatar: HTMLElement = createElem('div', styles['avatar__profile']);
 
-  avatar.style.backgroundImage = userStateAvatar ? `url(http://localhost:3000${userStateAvatar})` : `url(${user})`;
+  avatar.style.backgroundImage = userStateOnLoad?.avatarUrl
+    ? `url(http://localhost:3000${userStateOnLoad.avatarUrl})`
+    : `url(${user})`;
+
+  avatarWrap.style.display = userStateOnLoad?.parentControls === PARENT ? `flex` : `none`;
 
   avatarCircle.append(avatar);
   avatarWrap.append(avatarCircle);
@@ -35,17 +39,31 @@ export const renderAvatar = (): HTMLElement => {
 };
 
 export const renderChildAvatar = (text: string): HTMLElement => {
+  const userStateOnLoad = store.getState().user.personal.data;
+
   const avatarWrap: HTMLElement = createElem('div', 'avatar__wrapper');
   avatarWrap.classList.add('child-avatar__wrapper');
   const avatar: HTMLElement = createElem('div', styles['avatar__profile']);
   avatar.classList.add('child-avatar');
   const name = createElem('span', 'avatar__name');
-  name.innerHTML = text;
 
   const ageCnt = createElem('div', 'childe-age__cnt');
   const age = createElem('span', 'childe-age');
   age.innerHTML = '12+';
-  ageCnt.append(age);
+
+  avatarWrap.append(avatar, name);
+
+  if (userStateOnLoad?.parentControls === PARENT) {
+    name.innerHTML = text;
+    avatar.classList.remove('child-avatar-avtive');
+    ageCnt.innerHTML = '';
+  } else {
+    avatar.classList.add('child-avatar-avtive');
+    ageCnt.append(age);
+    name.innerHTML = '';
+
+    avatarWrap.append(ageCnt);
+  }
 
   avatarWrap.onclick = () => {
     const isCurrentChild = store.getState().user.personal.data?.parentControls === CHILD;
@@ -58,13 +76,12 @@ export const renderChildAvatar = (text: string): HTMLElement => {
     route(PATH_NAMES.main);
   }; // обновили стейт
 
-  avatarWrap.append(avatar, name);
-
   store.subscribe(() => {
     const userState = store.getState().user.personal;
 
     if (userState.data?.parentControls === CHILD) {
       avatar.classList.add('child-avatar-avtive');
+      ageCnt.append(age);
       avatarWrap.append(ageCnt);
       name.innerHTML = '';
     } else {
