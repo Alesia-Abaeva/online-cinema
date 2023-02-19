bcce830b66be58fa51b5a82ef9d0991028491953
lgklsv <kolosov20011@gmail.com>
2023-02-19 23:30:50 +0300

    feat: add three tests and improve jest config

--- client/src/utils/format-price.ts
+++ client/src/utils/format-price.ts
@@ -1,3 +1,4 @@
-export const formatPriceNum = (price: number | string): string => {
-  return price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ' ');
+export const formatPriceNum = (price: number): string => {
+  if (Number.isFinite(price)) return price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ' ');
+  return '';
 };
