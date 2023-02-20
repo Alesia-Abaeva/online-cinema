c7a000b66dca241669d24c1674ebfd6066e96683
gamesam88 <dyxpp@rambler.com>
2023-02-20 16:16:08 +0300

    feat: add compilations

--- client/src/const/top10-data.ts
+++ client/src/const/top10-data.ts
@@ -2,63 +2,63 @@ import { num1, num2, num3, num4, num5, num6, num7, num8, num9, num10 } from './i
 
 export const TOP10: IitemTop10[] = [
   {
-    id: 4426319,
-    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_4426319.jpg',
-    displayedTitle: 'Нападение на Рио Браво',
-    numImg: num1,
-  },
-  {
-    id: 1003587,
-    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_1003587.jpg',
-    displayedTitle: 'Гамильтон',
-    numImg: num2,
-  },
-  {
-    id: 1339977,
-    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_1339977.jpg',
-    displayedTitle: 'Уровень тревоги: Полночь',
-    numImg: num3,
-  },
-  {
-    id: 326,
-    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_326.jpg',
-    displayedTitle: 'Побег из Шоушенка',
-    numImg: num4,
-  },
-  {
     id: 435,
     img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_435.jpg',
     displayedTitle: 'Зеленая миля',
-    numImg: num5,
-  },
-  {
-    id: 734128,
-    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_734128.jpg',
-    displayedTitle: 'Время не терпит',
-    numImg: num6,
-  },
-  {
-    id: 4826246,
-    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_4826246.jpg',
-    displayedTitle: 'The Beatles: Get Back — Концерт на крыше',
-    numImg: num7,
-  },
-  {
-    id: 448,
-    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_448.jpg',
-    displayedTitle: 'Форрест Гамп',
-    numImg: num8,
-  },
-  {
-    id: 982493,
-    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_982493.jpg',
-    displayedTitle: 'Друзья: Ещё один эпизод перед последним — Десять лет Друзей',
-    numImg: num9,
+    numImg: num1,
   },
   {
     id: 329,
     img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_329.jpg',
     displayedTitle: 'Список Шиндлера',
+    numImg: num2,
+  },
+  {
+    id: 326,
+    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_326.jpg',
+    displayedTitle: 'Побег из Шоушенка',
+    numImg: num3,
+  },
+  {
+    id: 448,
+    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_448.jpg',
+    displayedTitle: 'Форрест Гамп',
+    numImg: num4,
+  },
+  {
+    id: 679486,
+    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_679486.jpg',
+    displayedTitle: 'Тайна Коко',
+    numImg: num5,
+  },
+  {
+    id: 3498,
+    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_3498.jpg',
+    displayedTitle: 'Властелин колец: Возвращение короля',
+    numImg: num6,
+  },
+  {
+    id: 258687,
+    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_258687.jpg',
+    displayedTitle: 'Интерстеллар',
+    numImg: num7,
+  },
+  {
+    id: 749374,
+    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_749374.jpg',
+    displayedTitle: 'Атака титанов',
+    numImg: num8,
+  },
+  {
+    id: 476,
+    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_476.jpg',
+    displayedTitle: 'Назад в будущее',
+    numImg: num9,
+  },
+  {
+    id: 342,
+    img: 'https://st.kp.yandex.net/images/film_iphone/iphone360_342.jpg',
+    displayedTitle: 'Криминальное чтиво',
     numImg: num10,
   },
 ];
