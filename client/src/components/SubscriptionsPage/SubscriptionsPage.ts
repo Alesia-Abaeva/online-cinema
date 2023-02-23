f13dd803354f134a3d81d1d3735ecca47c91764a
Alesia <alesia.litvinenko@gmail.com>
2023-02-23 18:01:15 +0300

    fix: change title in subsc. page

--- client/src/components/SubscriptionsPage/SubscriptionsPage.ts
+++ client/src/components/SubscriptionsPage/SubscriptionsPage.ts
@@ -1,16 +1,20 @@
-import { SUBSCRIPTION_PLANS } from 'src/const/subscriptions-data';
+import { SUBSCRIPTION_PLANS, Tariff } from 'src/const/subscriptions-data';
+import { store } from 'src/logic/redux';
 import { createElem } from 'src/utils/create-element';
 import { renderTariffCard } from './components/TariffCard/TariffCard';
 import styles from './SubscriptionsPage.module.scss';
 
 export const renderSubscriptions = (): HTMLElement => {
+  const tariff = store.getState().user.personal.data?.tariff;
+
   const main: HTMLElement = createElem('div', 'main');
   main.classList.add('main_banner');
   const mainContainer: HTMLElement = createElem('div', 'main__container');
   const mainContent: HTMLElement = createElem('div', styles['subscriptions']);
 
   const subsTitle: HTMLElement = createElem('h1', 'subscriptions__title');
-  subsTitle.innerHTML = 'Дружочек, надо сначала подписочку оформить!';
+  subsTitle.innerHTML =
+    tariff === Tariff.BASE ? 'Дружочек, надо сначала подписочку оформить!' : 'Примиальнее тебя, нет никого!';
   const subsTable: HTMLElement = createElem('div', 'subscriptions__body');
 
   SUBSCRIPTION_PLANS.forEach((el, id) => {
