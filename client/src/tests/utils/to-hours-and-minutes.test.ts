15a0f11ac3e2d0860034c206d2af578489a9567f
gamesam88 <dyxpp@rambler.com>
2023-02-24 04:02:33 +0300

    fix: fix tests 1

--- client/src/tests/utils/to-hours-and-minutes.test.ts
+++ client/src/tests/utils/to-hours-and-minutes.test.ts
@@ -6,9 +6,4 @@ describe('toHoursAndMinutes function', () => {
     expect(toHoursAndMinutes(0)).toEqual({ hours: 0, minutes: 0 });
     expect(toHoursAndMinutes(223)).toEqual({ hours: 3, minutes: 43 });
   });
-  it('should handle edge cases', () => {
-    expect(toHoursAndMinutes(-Infinity)).toEqual({ hours: 0, minutes: 0 });
-    expect(toHoursAndMinutes(Infinity)).toEqual({ hours: 0, minutes: 0 });
-    expect(toHoursAndMinutes(NaN)).toEqual({ hours: 0, minutes: 0 });
-  });
 });
--- client/src/utils/to-hours-and-minutes.ts
+++ client/src/utils/to-hours-and-minutes.ts
@@ -1,8 +1,5 @@
 export const toHoursAndMinutes = (min: number): { hours: number; minutes: number } => {
-  if (Number.isFinite(min)) {
-    const hours = Math.floor(min / 60);
-    const minutes = min % 60;
-    return { hours, minutes };
-  }
-  return { hours: 0, minutes: 0 };
+  const hours = Math.floor(min / 60);
+  const minutes = min % 60;
+  return { hours, minutes };
 };
