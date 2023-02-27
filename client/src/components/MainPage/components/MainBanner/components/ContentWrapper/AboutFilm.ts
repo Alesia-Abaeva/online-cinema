1f439a92e11e6aa3efa6909f2969e773c7789984
lgklsv <kolosov20011@gmail.com>
2023-02-27 14:58:06 +0300

    fix: delete comments

--- client/src/components/MainPage/components/MainBanner/components/ContentWrapper/AboutFilm.ts
+++ client/src/components/MainPage/components/MainBanner/components/ContentWrapper/AboutFilm.ts
@@ -18,11 +18,8 @@ export const renderAboutFilm = (
   const buttons: HTMLElement = createElem('div', styles.contentWrapper__actions);
   const content: HTMLElement = createElem('div', styles.contentWrapper);
 
-  // const filmImg = res.backdrop ? res.backdrop.url : '';
-
   const btnWatch = createBtnWatch(res.id) as HTMLButtonElement;
   const btnTrailer = createBtnTrailer(res) as HTMLButtonElement;
-  // const btnBookmark = createBtnBookmark() as HTMLButtonElement;
   btnSlice2.append(btnTrailer);
 
   if (dotsBtn) {
